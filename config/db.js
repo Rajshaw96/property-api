5e72ad0125e6871429951a21206d271d37b01bee
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-09-01 20:17:41 +0530

    Changes In API

--- config/db.js
+++ config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Exit process with failure
   }
 };
 
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const propertyRoutes = require('./routes/propertyRoutes');
 const connectRoutes = require('./routes/connectRoutes');
 const helmet = require('helmet');
-
 require('dotenv').config();
 
 const app = express();
+
+// Connect to MongoDB
 connectDB();
 
 app.use(helmet());
-app.use(bodyParser.json());
+app.use(express.json()); // Replacing bodyParser with Express's built-in middleware
 
+// Route handling
 app.use('/api/properties', propertyRoutes);
 app.use('/api/connect', connectRoutes);
 
