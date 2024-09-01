a43654b76158aeb3c181b3ca5c8bb8c7288f0bb0
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-09-01 19:46:21 +0530

    Changes In API

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,29 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+
+const app = express();
+
+// Retrieve the MongoDB URI from Firebase environment variables
 const mongoURI = functions.config().mongodb.uri;
 
-const admin = require("firebase-admin");
-admin.initializeApp();
-const db = admin.firestore();
+// Check if mongoURI is undefined and handle it
+if (!mongoURI) {
+  console.error("MongoDB URI is undefined.");
+  process.exit(1);  // Exit the process with failure
+}
 
+// Connect to MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-// Your Express routes go here
+// Define your Express routes here
 app.get("/api", (req, res) => {
   res.send("Hello from Firebase!");
 });
 
-// Expose Express API as a single Cloud Function:
+// Expose Express API as a single Cloud Function
 exports.api = functions.https.onRequest(app);
-
-mongoose.connect("mongodb+srv://admin:O7aAojvwFUoo7YsN@cluster0.mskks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }).then(() => console.log("MongoDB connected"))
-          .catch(err => console.log(err));
-        
